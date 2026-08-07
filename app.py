bb199057d13fb6591c59867d35d758f3cb8a59b4
akennedy-sde <akennedy@sde.idaho.gov>
2026-08-07 14:35:45 -0600

    Update app.py

--- app.py
+++ app.py
@@ -5,9 +5,7 @@ import streamlit.components.v1 as components
 # 1. Load the JSON in Python
 with open("embeddings.json", "r") as f:
     embeddings_data = f.read()  # Raw JSON string
-
-st.set_page_config(layout="wide")
-
+    
 # Load and display your exact HTML file
 with open("grant_lookup_tool.html", "r", encoding="utf-8") as f:
     html_code = f.read()
